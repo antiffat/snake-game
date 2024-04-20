4944176ac0c3fa5c195009f4ca7a92e13c20713b
s28222 <s28222@pjwstk.edu.pl>
2024-04-20 20:46:24 +0200

    main.py user input requests

---
+++ src/main.py
@@ -0,0 +1,4 @@
+def main():
+    width = int(input("Enter the width of the game board (5 to 25): "))
+    height = int(input("Enter the height of the game board (5 to 25): "))
+    player_name = input("Enter your name: ")
