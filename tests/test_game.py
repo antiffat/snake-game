f0376251b79ee6dfd5bbcff32e24c4f2ee3a4a21
s28222 <s28222@pjwstk.edu.pl>
2024-04-21 20:25:58 +0200

    test snake movement

--- tests/test_game.py
+++ tests/test_game.py
@@ -17,3 +17,9 @@ def test_food_not_on_snake():
     game = Game(10, 10, "Tester")
     assert game.food not in game.snake, "Food should not spawn on the snake."
 
+# test snake movement
+def test_snake_movement():
+    game = Game(10, 10, "Tester")
+    initial_head = game.snake[0]
+    game.move_snake('RIGHT')
+    assert game.snake[0] == (initial_head[0] + 1, initial_head[1]), "Snake should move right correctly."
