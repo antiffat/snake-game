cea32842b1d5d1ce07429f5aa7e69dc6466c22ba
s28222 <s28222@pjwstk.edu.pl>
2024-04-21 03:25:16 +0200

    test food spawning not on the snake

--- tests/test_game.py
+++ tests/test_game.py
@@ -11,3 +11,9 @@ def test_snake_initial_position():
     game = Game(10, 10, "Tester")
     expected_position = [(5, 5)]
     assert game.snake == expected_position, "Snake should start in the center of the board."
+
+# test food spawning not on the snake
+def test_food_not_on_snake():
+    game = Game(10, 10, "Tester")
+    assert game.food not in game.snake, "Food should not spawn on the snake."
+
